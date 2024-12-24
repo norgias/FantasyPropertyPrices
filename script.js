c4791b8a964e95dfd86660fe57840e475fd43741
Nicolas Orgias <norgias@gmail.com>
2024-12-24 06:29:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+fetch('./data/north_america.json')
+    .then((response) => response.json())
+    .then((data) => {
+        const currentCitySelect = document.getElementById('current-city');
+        const destinationCitySelect = document.getElementById('destination-city');
 
+        data.cities.forEach((city) => {
+            const option = document.createElement('option');
+            option.value = city.name;
+            option.textContent = city.name;
+            currentCitySelect.appendChild(option);
+
+            const destinationOption = option.cloneNode(true);
+            destinationCitySelect.appendChild(destinationOption);
+        });
+    })
+    .catch((error) => console.error('Error loading JSON:', error));
