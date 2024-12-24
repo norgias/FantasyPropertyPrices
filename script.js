0da5c8ca79cda88a5e4e7eac1608d5f2e1175504
Nicolas Orgias <norgias@gmail.com>
2024-12-24 07:44:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-fetch('./https://norgias.github.io/FantasyPropertyPrices/city_data/north_america.json')
-    .then((response) => response.json())
+fetch('https://norgias.github.io/FantasyPropertyPrices/city_data/north_america.json')
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+    })
     .then((data) => {
         const currentCitySelect = document.getElementById('current-city');
         const destinationCitySelect = document.getElementById('destination-city');
 
         data.cities.forEach((city) => {
+            // Create option for current city select dropdown
             const option = document.createElement('option');
-            option.value = city.name;
-            option.textContent = city.name;
+            option.value = city.city; // Use 'city.city' to get the name
+            option.textContent = city.city;
             currentCitySelect.appendChild(option);
 
+            // Clone and append to destination city dropdown
             const destinationOption = option.cloneNode(true);
             destinationCitySelect.appendChild(destinationOption);
         });
