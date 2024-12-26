5f19c5f0907c80be143049a83d24f31b71fe4905
Nicolas Orgias <norgias@gmail.com>
2024-12-25 20:45:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Fetch JSON data and populate city dropdowns
-fetch('https://norgias.github.io/FantasyPropertyPrices/city_data/north_america.json')
+fetch('city_data/north_america.json')
     .then((response) => {
         if (!response.ok) throw new Error('Failed to load JSON data');
         return response.json();
